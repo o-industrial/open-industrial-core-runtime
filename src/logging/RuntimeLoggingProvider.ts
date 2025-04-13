36d4c960e8a657455a7fb4aa809362c0132deb26
mcgear <michael.gearhardt@gmail.com>
2025-04-12 20:10:23 -0600

    init

---
+++ src/logging/RuntimeLoggingProvider.ts
@@ -0,0 +1,9 @@
+import { EaCApplicationsLoggingProvider } from '@fathym/eac-applications/runtime/logging';
+
+export class RuntimeLoggingProvider extends EaCApplicationsLoggingProvider {
+  constructor() {
+    const loggingPackages = ['@o-industrial/common', '@o-industrial/open-industrial-core-runtime'];
+
+    super(loggingPackages);
+  }
+}
